43298b3143d42c2ac7a37ba618726751fc59612c
Nishant <srnikhil3721@gmail.com>
2023-02-06 01:30:48 +0530

    find unique - optimized approach

--- simple-data-structure/arrays/4find-unique.cpp
+++ simple-data-structure/arrays/4find-unique.cpp
@@ -1,23 +1,38 @@
 // Brute force approach
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int array[] = {1, 4, 3, 1, 4, 3, 7};
+//     int occurrence[100] = {0};
+//     for (int i = 0; i < 7; i++)
+//     {
+//         occurrence[array[i]]++;
+//     }
+
+//     for (int i = 0; i < 100; i++)
+//     {
+//         if (occurrence[i] == 1)
+//         {
+//             cout << i << endl;
+//             break;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// Optimized approach
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int array[] = {1, 4, 3, 1, 4, 3, 7};
-    int occurrence[100] = {0};
+    int ans = 0;
     for (int i = 0; i < 7; i++)
     {
-        occurrence[array[i]]++;
+        ans = ans ^ array[i];
     }
-
-    for (int i = 0; i < 100; i++)
-    {
-        if (occurrence[i] == 1)
-        {
-            cout << i << endl;
-            break;
-        }
-    }
-
+    cout << ans << endl;
     return 0;
 }
