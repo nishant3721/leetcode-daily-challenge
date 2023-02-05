79b1294c4375d26f8e2989dc3b0ce7f867b59dbc
Nishant <srnikhil3721@gmail.com>
2023-02-05 19:26:25 +0530

    power of two

---
+++ simple-data-structure/arrays/2power-of-two.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+        return false;
+    while (n != 1)
+    {
+        if (n % 2 != 0)
+            return false;
+        n = n / 2;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << isPowerOfTwo(n) << endl;
+    return 0;
+}
